88d5d932aa164606c6cbb885a203673736d24043
HP <Mazer30@yandex.ru>
2024-03-15 17:20:56 +0400

    Модели данных

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,18 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.Duration;
+import java.time.LocalDate;
+
+
+@Data
 public class Film {
+    int id;
+    String name;
+    String description;
+    LocalDate releaseDate;
+    Duration duration;
 }
