2b73ae94a0c5946462e7cd079d819bc31117611c
nulome <Mazer30@yandex.ru>
2024-03-17 16:13:33 +0400

    Style : ревью

--- src/main/java/ru/yandex/practicum/filmorate/related/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/related/ValidationException.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import java.io.IOException;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-public class ValidationException extends Exception{
+public class ValidationException extends Exception {
     public ValidationException() {
     }
 
