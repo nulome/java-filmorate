a4219f1abff9e71cc5c7cc18e6bc40106f28191e
HP <Mazer30@yandex.ru>
2024-04-14 02:34:26 +0400

    style : style

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmRepositoryImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmRepositoryImpl.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.storage.film;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Primary;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
